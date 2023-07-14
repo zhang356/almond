9192570c3e59cd6005e82a86c9891841a65fff72
Lindsey Liu <lindseyliu@Lingzis-Mac-mini.local>
2023-07-14 08:55:40 -0700

    add speech to text

--- android/app/src/main/java/app/almondally/MainActivity.kt
+++ android/app/src/main/java/app/almondally/MainActivity.kt
@@ -1,26 +1,141 @@
 package app.almondally
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
 import app.almondally.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var speechRecognizer : SpeechRecognizer
+    private lateinit var speechRecognizerIntent : Intent
+
+    var TAG = "DEBUG"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
+
+        // Register the permissions callback, which handles the user's response to the
+// system permissions dialog. Save the return value, an instance of
+// ActivityResultLauncher. You can use either a val, as shown in this snippet,
+// or a lateinit var in your onAttach() or onCreate() method.
+        val requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    // Permission is granted. Continue the action or workflow in your
+                    // app.
+                    Log.v("DEBUG", "record audio permission granted")
+
+                } else {
+                    // Explain to the user that the feature is unavailable because the
+                    // feature requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
+                    Log.v("DEBUG", "record audio permission denied")
+                }
+            }
+
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // You can use the API that requires the permission.
+                Log.v(TAG, "record audio permission granted")
+            }
+            //shouldShowRequestPermissionRationale(...) -> {
+            // In an educational UI, explain to the user why your app requires this
+            // permission for a specific feature to behave as expected, and what
+            // features are disabled if it's declined. In this UI, include a
+            // "cancel" or "no thanks" button that lets the user continue
+            // using your app without granting the permission.
+            //showInContextUI(...)
+
+            else -> {
+                // You can directly ask for the permission.
+                // The registered ActivityResultCallback gets the result of this request.
+                requestPermissionLauncher.launch(
+                    Manifest.permission.RECORD_AUDIO)
+            }
+        }
+
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
+        speechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, /*Locale.getDefault()*/"en-US")
+
+        val speechRecognitionListener: RecognitionListener = object : RecognitionListener {
+            override fun onResults(results: Bundle) {
+                val voiceResults =
+                    results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+
+                if (voiceResults == null) {
+                    Log.e(TAG, "No voice results")
+                } else {
+                    Log.d(TAG, "Printing matches: ")
+                    for (match in voiceResults) {
+                        Log.d(TAG, match!!)
+                    }
+                    val bestMatch = voiceResults[0]
+                    // do something about the best match
+                }
+            }
+
+            override fun onReadyForSpeech(params: Bundle) {
+                Log.d(TAG, "Ready for speech")
+            }
+
+            override fun onError(error: Int) {
+                Log.d(TAG, "Error listening for speech: $error")
+            }
+
+            override fun onBeginningOfSpeech() {
+                Log.d(TAG, "Speech starting")
+            }
+
+            override fun onBufferReceived(buffer: ByteArray) {
+                // no-op
+            }
+
+            override fun onEndOfSpeech() {
+                Log.d(TAG, "Speech ends")
+            }
+
+            override fun onEvent(eventType: Int, params: Bundle) {
+                // no-op
+            }
+
+            override fun onPartialResults(partialResults: Bundle) {
+                // no-op
+            }
+
+            override fun onRmsChanged(rmsdB: Float) {
+                // no-op
+            }
+        }
+        speechRecognizer.setRecognitionListener(speechRecognitionListener)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -61,11 +176,12 @@ class MainActivity : AppCompatActivity() {
     private fun onStartStopButtonTapped(item: MenuItem) {
         if (item.title == resources.getString(R.string.start)) {
             //start recording
-
+            speechRecognizer.startListening(speechRecognizerIntent)
             //change text to stop
             item.title = resources.getString(R.string.stop)
         } else if (item.title == resources.getString(R.string.stop)) {
             //stop recording
+            speechRecognizer.stopListening()
 
             //change text to start
             item.title = resources.getString(R.string.start)
